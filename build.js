21c109c20f6e91b841842bb9b60178eed7072650
Manny <2204837+d3liriu5@users.noreply.github.com>
2023-08-23 01:44:24 -0700

    Create build.js

---
+++ build.js
@@ -0,0 +1,66 @@
+const StyleDictionaryPackage = require('style-dictionary');
+
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+
+function getStyleDictionaryConfig(brand, platform) {
+  return {
+    "source": [
+      `input/brands/${brand}/*.json`,
+      "input/globals/**/*.json"
+    ],
+    "platforms": {
+      "web": {
+        "transformGroup": "web",
+        "buildPath": `output/web/${brand}/`,
+        "files": [{
+          "destination": "tokens.scss",
+          "format": "css/variables"
+        }]
+      },
+      "android": {
+        "transformGroup": "android",
+        "buildPath": `output/android/${brand}/`,
+        "files": [{
+          "destination": "tokens.colors.xml",
+          "format": "android/colors"
+        },{
+          "destination": "tokens.dimens.xml",
+          "format": "android/dimens"
+        },{
+          "destination": "tokens.font_dimens.xml",
+          "format": "android/fontDimens"
+        }]
+      },
+      "ios": {
+        "transformGroup": "ios",
+        "buildPath": `output/ios/${brand}/`,
+        "files": [{
+          "destination": "tokens.h",
+          "format": "ios/macros"
+        }]
+      }
+    }
+  };
+}
+
+console.log('Build started...');
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
+
+['light', 'dark'].map(function (brand) {
+  ['web', 'ios', 'android'].map(function (platform) {
+
+    console.log('\n==============================================');
+    console.log(`\nProcessing: [${platform}] [${brand}]`);
+
+    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(brand, platform));
+
+    StyleDictionary.buildPlatform(platform);
+
+    console.log('\nEnd processing');
+
+  })
+})
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
