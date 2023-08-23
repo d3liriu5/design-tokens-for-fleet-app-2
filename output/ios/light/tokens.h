0c833c8be505fb9768f19c240b0f85481dd2ecf3
d3liriu5 <d3liriu5@users.noreply.github.com>
2023-08-23 10:22:44 +0000

    Apply automatic changes

---
+++ output/ios/light/tokens.h
@@ -0,0 +1,83 @@
+
+//
+// tokens.h
+//
+
+// Do not edit directly
+// Generated on Wed, 23 Aug 2023 10:22:43 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define PRefColorGrey3 #f7f7f7
+#define PRefColorGrey5 #f2f2f2
+#define PRefColorGrey10 #E3E3E3
+#define PRefColorGrey20 #C7C7C7
+#define PRefColorGrey30 #ABABAB
+#define PRefColorGrey40 #919191
+#define PRefColorGrey50 #757575
+#define PRefColorGrey60 #5E5E5E
+#define PRefColorGrey70 #454545
+#define PRefColorGrey80 #2E2E2E
+#define PRefColorGrey90 #171717
+#define PRefColorWhite100 #FFFFFF
+#define PRefColorBlack100 #000000
+#define PRefColorBlue10 #C6E8FF
+#define PRefColorBlue40 #0094FF
+#define PRefColorBlue50 #0073E7
+#define PRefColorGreen30 #32C353
+#define PRefColorGreen40 #28A648
+#define PRefColorOrange30 #FF8C40
+#define PRefColorRed5 #FFECEA
+#define PRefColorRed50 #EA1000
+#define PRefColorTeal5 #E5F6F4
+#define PRefFontWeightMedium 500
+#define PRefFontWeightRegular 400
+#define PRefFontWeightSemibold 600
+#define PRefFontWeightBold 700
+#define PRefBorderWidth1 1
+#define PRefBorderRadius1 4
+#define PRefBorderRadius2 8
+#define PRefBorderRadius3 12
+#define PRefBorderRadius4 16
+#define PRefBorderRadiusFull 9999
+#define PRefSpacing1 4
+#define PRefSpacing2 8
+#define PRefSpacing3 12
+#define PRefSpacing4 16
+#define PRefSpacing5 20
+#define PRefSpacing6 24
+#define PRefSpacing7 28
+#define PRefSpacing8 32
+#define PRefSpacingEdgeMargin 20
+#define PRefSpacingButtonDefault 12 16 12 16
+#define PRefSpacingRowNotifications 12 20 16 20
+#define PRefSpacingRowTopnav 12 16 12 16
+#define PRefFontFamily Inter
+#define PRefFontSize075 12
+#define PRefFontSize0875 14
+#define PRefFontSize10 16
+#define PRefFontSize1125 18
+#define PRefFontSize125 20
+#define PRefFontSize15 24
+#define PRefFontSize175 28
+#define PRefFontSize20 32
+#define PRefLineHeight10 16
+#define PRefLineHeight125 20
+#define PRefLineHeight15 24
+#define PRefLineHeight175 28
+#define PRefLineHeight20 32
+#define PFleetappTypeHeadingDefault [object Object]
+#define PFleetappTypeHeadingMedium [object Object]
+#define PFleetappTypeHeadingSmall [object Object]
+#define PFleetappTypeHeadingXsmall [object Object]
+#define PFleetappTypeBodyDefault [object Object]
+#define PFleetappTypeBodyListitem [object Object]
+#define PFleetappTypeBodySelector [object Object]
+#define PFleetappTypeBodySelectoractive [object Object]
+#define PFleetappTypeLabelDefault [object Object]
+#define PFleetappTypeButtonDefault [object Object]
+#define PFleetappTypeButtonSubdued [object Object]
+#define PFleetappTypeButtonInline [object Object]
+
