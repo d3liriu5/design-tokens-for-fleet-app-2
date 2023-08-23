b140f630d7265c4ce03ee66e6775803bf2bb3511
d3liriu5 <d3liriu5@users.noreply.github.com>
2023-08-23 10:26:50 +0000

    Apply automatic changes

--- output/ios/dark/tokens.h
+++ output/ios/dark/tokens.h
@@ -4,12 +4,57 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 23 Aug 2023 10:22:43 GMT
+// Generated on Wed, 23 Aug 2023 10:26:49 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#define PFleetappColorBgDefault #000000
+#define PFleetappColorBgSubdued #171717
+#define PFleetappColorBgInput #454545
+#define PFleetappColorBgCard #2E2E2E
+#define PFleetappColorBgCardhighlight #000000
+#define PFleetappColorBgPrimary #0094FF
+#define PFleetappColorBgSecondary #f2f2f2
+#define PFleetappColorBgActive #FFFFFF
+#define PFleetappColorBgAvatar #454545
+#define PFleetappColorBgInputalt #454545
+#define PFleetappColorBgHighlight #2E2E2E
+#define PFleetappColorBgCritical #FFECEA
+#define PFleetappColorBgDisabled #f2f2f2
+#define PFleetappColorTextDefault #FFFFFF
+#define PFleetappColorTextSubdued #ABABAB
+#define PFleetappColorTextHighlightactive #0094FF
+#define PFleetappColorTextInteractive #0094FF
+#define PFleetappColorTextPlaceholder #919191
+#define PFleetappColorTextInverse #171717
+#define PFleetappColorTextCritical #EA1000
+#define PFleetappColorTextOnBold #FFFFFF
+#define PFleetappColorTextOnHighlight #C7C7C7
+#define PFleetappColorBorderDefault #454545
+#define PFleetappColorBorderInteractive #0094FF
+#define PFleetappColorIconDefault #FFFFFF
+#define PFleetappColorIconInteractive #0094FF
+#define PFleetappColorIconSuccess #28A648
+#define PFleetappColorIconWarning #FF8C40
+#define PFleetappColorIconCritical #EA1000
+#define PFleetappColorIconSubdued #919191
+#define PFleetappColorIconActive #28A648
+#define PFleetappColorIconInactive #454545
+#define PFleetappColorIconOnBold #FFFFFF
+#define PFleetappTypeHeadingDefault [object Object]
+#define PFleetappTypeHeadingMedium [object Object]
+#define PFleetappTypeHeadingSmall [object Object]
+#define PFleetappTypeHeadingXsmall [object Object]
+#define PFleetappTypeBodyDefault [object Object]
+#define PFleetappTypeBodyListitem [object Object]
+#define PFleetappTypeBodySelector [object Object]
+#define PFleetappTypeBodySelectoractive [object Object]
+#define PFleetappTypeLabelDefault [object Object]
+#define PFleetappTypeButtonDefault [object Object]
+#define PFleetappTypeButtonSubdued [object Object]
+#define PFleetappTypeButtonInline [object Object]
 #define PRefColorGrey3 #f7f7f7
 #define PRefColorGrey5 #f2f2f2
 #define PRefColorGrey10 #E3E3E3
@@ -68,16 +113,4 @@
 #define PRefLineHeight15 24
 #define PRefLineHeight175 28
 #define PRefLineHeight20 32
-#define PFleetappTypeHeadingDefault [object Object]
-#define PFleetappTypeHeadingMedium [object Object]
-#define PFleetappTypeHeadingSmall [object Object]
-#define PFleetappTypeHeadingXsmall [object Object]
-#define PFleetappTypeBodyDefault [object Object]
-#define PFleetappTypeBodyListitem [object Object]
-#define PFleetappTypeBodySelector [object Object]
-#define PFleetappTypeBodySelectoractive [object Object]
-#define PFleetappTypeLabelDefault [object Object]
-#define PFleetappTypeButtonDefault [object Object]
-#define PFleetappTypeButtonSubdued [object Object]
-#define PFleetappTypeButtonInline [object Object]
 
--- output/ios/light/tokens.h
+++ output/ios/light/tokens.h
@@ -4,12 +4,57 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 23 Aug 2023 10:22:43 GMT
+// Generated on Wed, 23 Aug 2023 10:26:49 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+#define PFleetappColorBgDefault #FFFFFF
+#define PFleetappColorBgSubdued #f7f7f7
+#define PFleetappColorBgInput #FFFFFF
+#define PFleetappColorBgCard #FFFFFF
+#define PFleetappColorBgCardhighlight #f2f2f2
+#define PFleetappColorBgPrimary #0073E7
+#define PFleetappColorBgSecondary #f2f2f2
+#define PFleetappColorBgActive #171717
+#define PFleetappColorBgAvatar #E3E3E3
+#define PFleetappColorBgInputalt #f2f2f2
+#define PFleetappColorBgHighlight #E5F6F4
+#define PFleetappColorBgCritical #EA1000
+#define PFleetappColorBgDisabled #f2f2f2
+#define PFleetappColorTextDefault #171717
+#define PFleetappColorTextSubdued #757575
+#define PFleetappColorTextHighlightactive #0073E7
+#define PFleetappColorTextInteractive #0073E7
+#define PFleetappColorTextPlaceholder #ABABAB
+#define PFleetappColorTextInverse #FFFFFF
+#define PFleetappColorTextCritical #EA1000
+#define PFleetappColorTextOnBold #FFFFFF
+#define PFleetappColorTextOnHighlight #5E5E5E
+#define PFleetappColorBorderDefault #E3E3E3
+#define PFleetappColorBorderInteractive #0073E7
+#define PFleetappColorIconDefault #FFFFFF
+#define PFleetappColorIconInteractive #0073E7
+#define PFleetappColorIconSuccess #32C353
+#define PFleetappColorIconWarning #FF8C40
+#define PFleetappColorIconCritical #EA1000
+#define PFleetappColorIconSubdued #919191
+#define PFleetappColorIconActive #32C353
+#define PFleetappColorIconInactive #E3E3E3
+#define PFleetappColorIconOnBold #FFFFFF
+#define PFleetappTypeHeadingDefault [object Object]
+#define PFleetappTypeHeadingMedium [object Object]
+#define PFleetappTypeHeadingSmall [object Object]
+#define PFleetappTypeHeadingXsmall [object Object]
+#define PFleetappTypeBodyDefault [object Object]
+#define PFleetappTypeBodyListitem [object Object]
+#define PFleetappTypeBodySelector [object Object]
+#define PFleetappTypeBodySelectoractive [object Object]
+#define PFleetappTypeLabelDefault [object Object]
+#define PFleetappTypeButtonDefault [object Object]
+#define PFleetappTypeButtonSubdued [object Object]
+#define PFleetappTypeButtonInline [object Object]
 #define PRefColorGrey3 #f7f7f7
 #define PRefColorGrey5 #f2f2f2
 #define PRefColorGrey10 #E3E3E3
@@ -68,16 +113,4 @@
 #define PRefLineHeight15 24
 #define PRefLineHeight175 28
 #define PRefLineHeight20 32
-#define PFleetappTypeHeadingDefault [object Object]
-#define PFleetappTypeHeadingMedium [object Object]
-#define PFleetappTypeHeadingSmall [object Object]
-#define PFleetappTypeHeadingXsmall [object Object]
-#define PFleetappTypeBodyDefault [object Object]
-#define PFleetappTypeBodyListitem [object Object]
-#define PFleetappTypeBodySelector [object Object]
-#define PFleetappTypeBodySelectoractive [object Object]
-#define PFleetappTypeLabelDefault [object Object]
-#define PFleetappTypeButtonDefault [object Object]
-#define PFleetappTypeButtonSubdued [object Object]
-#define PFleetappTypeButtonInline [object Object]
 
