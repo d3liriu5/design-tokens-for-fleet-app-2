92ce1c75bb87aaea47890a79f380d1e27a1f5c6c
d3liriu5 <d3liriu5@users.noreply.github.com>
2023-08-24 22:29:32 +0000

    Automated token transformation triggered by new commit

---
+++ output/compose/dark/StyleDictionaryColor.kt
@@ -0,0 +1,69 @@
+
+
+// Do not edit directly
+// Generated on Thu, 24 Aug 2023 22:29:32 GMT
+
+
+
+package StyleDictionaryColor;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionaryColor {
+  val pFleetappColorBgActive = #FFFFFF
+  val pFleetappColorBgAvatar = #454545
+  val pFleetappColorBgCard = #2E2E2E
+  val pFleetappColorBgCardhighlight = #000000
+  val pFleetappColorBgCritical = #FFECEA
+  val pFleetappColorBgDefault = #000000
+  val pFleetappColorBgDisabled = #f2f2f2
+  val pFleetappColorBgHighlight = #2E2E2E
+  val pFleetappColorBgInput = #454545
+  val pFleetappColorBgInputalt = #454545
+  val pFleetappColorBgPrimary = #0094FF
+  val pFleetappColorBgSecondary = #f2f2f2
+  val pFleetappColorBgSubdued = #171717
+  val pFleetappColorBorderDefault = #454545
+  val pFleetappColorBorderInteractive = #0094FF
+  val pFleetappColorIconActive = #28A648
+  val pFleetappColorIconCritical = #EA1000
+  val pFleetappColorIconDefault = #FFFFFF
+  val pFleetappColorIconInactive = #454545
+  val pFleetappColorIconInteractive = #0094FF
+  val pFleetappColorIconOnBold = #FFFFFF
+  val pFleetappColorIconSubdued = #919191
+  val pFleetappColorIconSuccess = #28A648
+  val pFleetappColorIconWarning = #FF8C40
+  val pFleetappColorTextCritical = #EA1000
+  val pFleetappColorTextDefault = #FFFFFF
+  val pFleetappColorTextHighlightactive = #0094FF
+  val pFleetappColorTextInteractive = #0094FF
+  val pFleetappColorTextInverse = #171717
+  val pFleetappColorTextOnBold = #FFFFFF
+  val pFleetappColorTextOnHighlight = #C7C7C7
+  val pFleetappColorTextPlaceholder = #919191
+  val pFleetappColorTextSubdued = #ABABAB
+  val pRefColorBlack100 = #000000
+  val pRefColorBlue10 = #C6E8FF
+  val pRefColorBlue40 = #0094FF
+  val pRefColorBlue50 = #0073E7
+  val pRefColorGreen30 = #32C353
+  val pRefColorGreen40 = #28A648
+  val pRefColorGrey10 = #E3E3E3
+  val pRefColorGrey20 = #C7C7C7
+  val pRefColorGrey3 = #f7f7f7
+  val pRefColorGrey30 = #ABABAB
+  val pRefColorGrey40 = #919191
+  val pRefColorGrey5 = #f2f2f2
+  val pRefColorGrey50 = #757575
+  val pRefColorGrey60 = #5E5E5E
+  val pRefColorGrey70 = #454545
+  val pRefColorGrey80 = #2E2E2E
+  val pRefColorGrey90 = #171717
+  val pRefColorOrange30 = #FF8C40
+  val pRefColorRed5 = #FFECEA
+  val pRefColorRed50 = #EA1000
+  val pRefColorTeal5 = #E5F6F4
+  val pRefColorWhite100 = #FFFFFF
+}
---
+++ output/compose/light/StyleDictionaryColor.kt
@@ -0,0 +1,69 @@
+
+
+// Do not edit directly
+// Generated on Thu, 24 Aug 2023 22:29:32 GMT
+
+
+
+package StyleDictionaryColor;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionaryColor {
+  val pFleetappColorBgActive = #171717
+  val pFleetappColorBgAvatar = #E3E3E3
+  val pFleetappColorBgCard = #FFFFFF
+  val pFleetappColorBgCardhighlight = #f2f2f2
+  val pFleetappColorBgCritical = #EA1000
+  val pFleetappColorBgDefault = #FFFFFF
+  val pFleetappColorBgDisabled = #f2f2f2
+  val pFleetappColorBgHighlight = #E5F6F4
+  val pFleetappColorBgInput = #FFFFFF
+  val pFleetappColorBgInputalt = #f2f2f2
+  val pFleetappColorBgPrimary = #0073E7
+  val pFleetappColorBgSecondary = #f2f2f2
+  val pFleetappColorBgSubdued = #f7f7f7
+  val pFleetappColorBorderDefault = #E3E3E3
+  val pFleetappColorBorderInteractive = #0073E7
+  val pFleetappColorIconActive = #32C353
+  val pFleetappColorIconCritical = #EA1000
+  val pFleetappColorIconDefault = #FFFFFF
+  val pFleetappColorIconInactive = #E3E3E3
+  val pFleetappColorIconInteractive = #0073E7
+  val pFleetappColorIconOnBold = #FFFFFF
+  val pFleetappColorIconSubdued = #919191
+  val pFleetappColorIconSuccess = #32C353
+  val pFleetappColorIconWarning = #FF8C40
+  val pFleetappColorTextCritical = #EA1000
+  val pFleetappColorTextDefault = #171717
+  val pFleetappColorTextHighlightactive = #0073E7
+  val pFleetappColorTextInteractive = #0073E7
+  val pFleetappColorTextInverse = #FFFFFF
+  val pFleetappColorTextOnBold = #FFFFFF
+  val pFleetappColorTextOnHighlight = #5E5E5E
+  val pFleetappColorTextPlaceholder = #ABABAB
+  val pFleetappColorTextSubdued = #757575
+  val pRefColorBlack100 = #000000
+  val pRefColorBlue10 = #C6E8FF
+  val pRefColorBlue40 = #0094FF
+  val pRefColorBlue50 = #0073E7
+  val pRefColorGreen30 = #32C353
+  val pRefColorGreen40 = #28A648
+  val pRefColorGrey10 = #E3E3E3
+  val pRefColorGrey20 = #C7C7C7
+  val pRefColorGrey3 = #f7f7f7
+  val pRefColorGrey30 = #ABABAB
+  val pRefColorGrey40 = #919191
+  val pRefColorGrey5 = #f2f2f2
+  val pRefColorGrey50 = #757575
+  val pRefColorGrey60 = #5E5E5E
+  val pRefColorGrey70 = #454545
+  val pRefColorGrey80 = #2E2E2E
+  val pRefColorGrey90 = #171717
+  val pRefColorOrange30 = #FF8C40
+  val pRefColorRed5 = #FFECEA
+  val pRefColorRed50 = #EA1000
+  val pRefColorTeal5 = #E5F6F4
+  val pRefColorWhite100 = #FFFFFF
+}
